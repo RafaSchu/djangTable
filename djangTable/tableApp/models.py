3dc956496cd6041dfd1fd3958cd544a03c5c5923
RafaSchu <94032887+RafaSchu@users.noreply.github.com>
2024-05-09 16:20:16 -0400

    Add Model and start import_csv

--- djangTable/tableApp/models.py
+++ djangTable/tableApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Person(models.Model):
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+
+    # Define other fields based on your columns
+    person_id = models.IntegerField(primary_key=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    age = models.IntegerField()
+    occupation = models.CharField(max_length=100)
+    sleep_duration = models.FloatField()
+    quality_sleep = models.BooleanField()
+    physical_activity_level = models.CharField(max_length=100)
+    stress_level = models.IntegerField()
+    bmi_category = models.CharField(max_length=50)
+    blood_pressure = models.CharField(max_length=50)
+    heart_rate = models.IntegerField()
+    daily_steps = models.IntegerField()
+    sleep_disorder = models.BooleanField()
+
+    # Automatically sets the field to now when the object is first created
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.person_id)
